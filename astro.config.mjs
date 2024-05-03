bf0fb0a898a6619e1eb3aa1ac4d2e44dbf004805
adAdministrator <Fimafimachenko@proton.me>
2024-05-03 17:15:20 +0300

    add

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,9 @@
 import { defineConfig } from 'astro/config';
-import { sitemap } from '@astrojs/sitemap';
-import { tailwind } from '@astrojs/tailwind';
-import { loadEnv } from "vite";
 
-const SERVER_PORT = 3000;
-const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
-const LIVE_URL = "https://adAdministrator.github.io";
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
 
-const SCRIPT = process.env.npm_lifecycle_script || "";
-const isBuild = SCRIPT.includes("astro build");
-
-let BASE_URL = LOCALHOST_URL;
-
-if (isBuild) {
-  BASE_URL = LIVE_URL;
-}
-
+// https://astro.build/config
 export default defineConfig({
-  server: {
-    port: SERVER_PORT
-  },
-  site: BASE_URL,
-  integrations: [
-    sitemap(),
-    tailwind({
-      config: { applyBaseStyles: false }
-    })
-  ]
+  integrations: [tailwind()]
 });
