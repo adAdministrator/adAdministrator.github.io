906108f20267c39778b2d6bdbcf6c3f493de6797
adAdministrator <Fimafimachenko@proton.me>
2024-05-03 15:32:48 +0300

    add commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,29 @@
 import { defineConfig } from 'astro/config';
+import { sitemap } from '@astrojs/sitemap';
+import { tailwind } from '@astrojs/tailwind';
 
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+const SERVER_PORT = 3000;
+const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
+const LIVE_URL = "https://adAdministrator.github.io";
 
-// https://astro.build/config
+const SCRIPT = process.env.npm_lifecycle_script || "";
+const isBuild = SCRIPT.includes("astro build");
+
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
-  integrations: [tailwind()]
+  server: {
+    port: SERVER_PORT
+  },
+  site: BASE_URL,
+  integrations: [
+    sitemap(),
+    tailwind({
+      config: { applyBaseStyles: false }
+    })
+  ]
 });
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('homepage has title and links', async ({ page }) => {
-  await page.goto('http://localhost:3001');
+  await page.goto('http://localhost:3000');
 
   await expect(page).toHaveTitle(/LinkTreeFreeClone/);
 
