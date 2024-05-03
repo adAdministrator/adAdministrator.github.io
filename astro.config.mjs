b5a897906e0b755a4c0b434b2cf7daf4092a49f8
adAdministrator <Fimafimachenko@proton.me>
2024-05-04 00:52:26 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adAdministrator.github.io',
-  base: 'https://github.com/adAdministrator/adAdministrator.github.io',
   integrations: [tailwind()]
 });
